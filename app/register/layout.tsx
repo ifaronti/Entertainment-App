8f12f3df9277d248dbc283991ea1d2b927f5a6c7
purplenimbus <anthony.akpan@hotmail.com>
2024-08-29 08:19:03 -0700

    update meta title

--- app/register/layout.tsx
+++ app/register/layout.tsx
@@ -4,7 +4,7 @@ import { Metadata } from "next";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "Log into your account",
+  title: "Create an account",
 };
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
