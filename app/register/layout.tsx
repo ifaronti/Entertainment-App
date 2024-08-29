f1ab8d309c30dbaada7f548b93696a68db5b5215
ifaronti <135629496+ifaronti@users.noreply.github.com>
2024-08-29 01:14:33 +0100

    register page layout finished

--- app/register/layout.tsx
+++ app/register/layout.tsx
@@ -1,16 +1,15 @@
 import { Metadata } from "next"
 import AppLogo from "@/components/AppLogo";
 import Container from "@/components/Container"
+import { registerStyles } from "./style";
+
 export const metadata: Metadata = {
     title: "Register to begin"
-  };
+};
+  
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-            
-            <Container className="min-h-[100vh] relative flex flex-col gap-[83px] items-center justify-center w-full">
+    return <Container className={registerStyles.layoutContainer}>
                 <AppLogo/>
                 {children}
-            </Container>
-
-    )
+           </Container>
 }
