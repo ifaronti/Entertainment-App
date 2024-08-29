073aeec8a201c258d7f7b5cd79e2be9c59b2fe0e
ifaronti <135629496+ifaronti@users.noreply.github.com>
2024-08-29 01:07:18 +0100

    Register Page layout Done!

--- app/register/layout.tsx
+++ app/register/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next"
 import AppLogo from "@/components/AppLogo";
 import Container from "@/components/Container"
 export const metadata: Metadata = {
-    title: "Register to begin",
+    title: "Register to begin"
   };
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
